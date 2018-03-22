1adca13ec0a4eb407b53a0aea57bcd1a62c06315
Иван Павликов <ivanp220598@gmail.com>
2018-03-22 14:41:42 +0300

    fixed cart button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-
+var cart_btn = document.getElementById('cart_menu_button');
 function cart_button() {
-	var cart_btn = document.getElementById('cart_btn');
-	console.log(cart_btn.classList.item);
-	cart_btn.onclick = cart_btn.classList.toggle('active');
+	cart_btn.classList.toggle('active');
 	// body...
 };
-
-cart_button();
